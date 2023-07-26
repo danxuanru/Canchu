a727cd02f22eb70d59ef2da0ccfb1d4e258a8852
danxuanru <danxuanru@gmail.com>
2023-07-26 17:01:25 +0800

    get client ip

--- project/router.js
+++ project/router.js
@@ -28,11 +28,19 @@ app.use(cors(corsOption));
 app.use(express.json());
 
 app.set('trust proxy', true);
-app.use(limiter);
+// app.use(limiter);
 
 // app.use('/.well-known/pki-validation/', express.static(__dirname + '/images'));
 
-// app.get('/ip', (request, response) => response.send(request.ip))
+app.get('/', (req, res) => {
+  // Get the IP address of the client making the request
+  const clientIP = req.ip;
+
+  // Get the IP address of the proxy (if available) from the 'x-forwarded-for' header
+  const proxyIP = req.headers['x-forwarded-for'];
+
+  res.send(`Client IP: ${clientIP}, Proxy IP: ${proxyIP}`);
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
