e4b12bdb11b053cdfffb3037fb9a564f8c4bc3dd
danxuanru <danxuanru@gmail.com>
2023-08-01 10:20:55 +0800

    modify router

--- project/Router/posts_router.js
+++ project/Router/posts_router.js
@@ -4,12 +4,12 @@ const { authenticateToken } = require('../utils/authorization');
 const { createPost, updatePost, createPostLike, deletePostLike, createPostComment, getPostDetail, postSearch } = require('../Controllers/posts');
 const router = express.Router();
 
-router.post('/', authenticateToken, createPost);
-router.put('/:id', authenticateToken, updatePost);
-router.post('/:id/like', authenticateToken, createPostLike);
-router.delete('/:id/like', authenticateToken, deletePostLike);
-router.post('/:id/comment', authenticateToken, createPostComment);
-router.get('/:id', authenticateToken, getPostDetail);
-router.get('/search', authenticateToken, postSearch);
+router.post('/posts/', authenticateToken, createPost);
+router.put('/posts/:id', authenticateToken, updatePost);
+router.post('/posts/:id/like', authenticateToken, createPostLike);
+router.delete('/posts/:id/like', authenticateToken, deletePostLike);
+router.post('/posts/:id/comment', authenticateToken, createPostComment);
+router.get('/posts/:id', authenticateToken, getPostDetail);
+router.get('/posts/search', authenticateToken, postSearch);
 
 module.exports = router;
