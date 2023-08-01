89d879b05aff044bac040190329981e1a35fa1ee
danxuanru <danxuanru@gmail.com>
2023-08-01 16:17:50 +0800

    clear profile cache after blocked

--- project/utils/rateLimiter.js
+++ project/utils/rateLimiter.js
@@ -1,30 +1,36 @@
 /* eslint-disable semi */
 // const rateLimit = require('express-rate-limit');
 const Redis = require('ioredis');
+const { clearCache } = require('./cache');
+const { getUserId } = require('./utils');
 const client = new Redis(); // port 6379
 const limit = 3;
 
 async function rateLimiter (req, res, next) {
   try {
+    const userId = getUserId();
+
     // get ip from header
-    // console.log(req.connection.remoteAddress + ' / ' + req.headers['x-forward-for'] + ' / ' + req.headers['x-real-ip']);
     const ip = req.headers['x-real-ip'] || req.headers['x-forward-for'] || req.connection.remoteAddress;
-    // get ip count & remain ttl
+
+    // get ip count & block or not
     const ipKey = `rateLimiter:ip#${ip}`;
-    console.log(ipKey);
     const blockKey = `rateLimiter:block#${ip}`;
+
     const blockRequest = await client.get(blockKey);
     let requestCount = await client.get(ipKey);
 
     if (!blockRequest && requestCount) {
       requestCount = parseInt(requestCount);
+
       if (requestCount >= limit) {
         // add to blocklist
         client.set(blockKey, 1, 'EX', 10);
         client.del(ipKey); // delete ipKey
+        clearCache(userId); // clear user profile cache
+
         return res.status(429).json({ error: `${ipKey} request too much!!` });
       }
-      console.log(`第${requestCount + 1}訪問 / s`);
       // await client.setex(ipKey, 1, parseInt(requestCount) + 1);
       await client.incr(ipKey);
 
