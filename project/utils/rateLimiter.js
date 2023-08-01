5f2d9b11043cf639d14d6e039c70ae1434a7c4e8
danxuanru <danxuanru@gmail.com>
2023-08-01 14:22:27 +0800

    ratelimiter

--- project/utils/rateLimiter.js
+++ project/utils/rateLimiter.js
@@ -8,21 +8,21 @@ async function rateLimiter (req, res) {
   try {
     // get ip from header
     console.log(req.ip + ' / ' + req.headers['x-forward-for'] + ' / ');
-    const ip = req.headers['x-forward-for'] || req.ip;
+    const ip = req.headers['x-forward-for'] || req
     // get ip count & remain ttl
     const ipKey = `rateLimiter:ip#${ip}`;
     console.log(ipKey);
-    const requestCount = client.get(ipKey);
-    const ttl = client.ttl(ipKey)
+    const requestCount = await client.get(ipKey);
+    const ttl = await client.ttl(ipKey)
     if (requestCount) {
       if (requestCount >= limit) {
         // add blacklist
         return res.status(429).json('請求過多了 請稍後再試!');
       }
-      console.log(`第${requestCount + 1}訪問 / s`);
-      client.setex(ipKey, ttl, parseInt(requestCount) + 1);
+      console.log(`第${parseInt(requestCount) + 1}訪問 / s`);
+      await client.setex(ipKey, ttl, parseInt(requestCount) + 1);
     } else { // no request in this second
-      client.set(ipKey, 1, 'EX', 1);
+      await client.set(ipKey, 1, 'EX', 1);
     }
   } catch (error) {
     console.error(error);
